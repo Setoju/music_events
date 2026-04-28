03f2420f85efc7eaeb9d11509dd1a5a8ce2adaeb
Setoju <setoju48@gmail.com>
2026-04-28 15:54:43 +0300

    feat: Configure autoload paths for API and enable CORS middleware

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module MusicEvents
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
 
+    config.paths.add "app/api", glob: "**/*.rb"
+    config.autoload_paths << Rails.root.join("app/api")
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
